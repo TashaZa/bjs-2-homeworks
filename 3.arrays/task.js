ad073a2c53a0f1e45bf3a91ee4614edc8554ed41
Наталья <lu_na87@mail.ru>
2022-10-04 23:12:31 +0300

    Домашнее задание 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,14 @@
+// Задача 1
+
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  let result = arr1.length === arr2.length && arr1.every((item,index) => arr2.indexOf(item) == index);
+  return result
 }
 
-function advancedFilter(arr) {
-  let resultArr;
+// Задача 2
 
-  // Ваш код
+function advancedFilter(arr) {
+  let resultArr = arr.filter(item => item >= 0 && item % 3 === 0).map(item => item * 10)
 
   return resultArr; // array
 }
