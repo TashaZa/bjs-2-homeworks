9a8a2095ea9419ef0da5eefb887c66469fc2ecf0
Наталья <lu_na87@mail.ru>
2022-10-04 22:56:00 +0300

    Домашнее задание 2

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,62 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+   let min = Infinity
+  let max = -Infinity
+  let sum = 0;
+  let avg;
 
-  // Ваш код
-
+  for (let i=0; i<arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i] 
+    }
+    if (arr[i] < min) {
+      min = arr[i]
+    }
+    sum = sum + arr[i]
+  }
+  avg = Number((sum/arr.length).toFixed(2))
   return { min: min, max: max, avg: avg };
 }
+console.log(getArrayParams([-85, 85, 26, 7, 47]))
+
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+   let sum = 0;
+   for (let i = 0; i < arr.length; i++) {sum = sum + arr[i]
+  }
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
-  let max;
+function makeWork(arrOfArr, worker) {
+  let max = 0;
 
-  // Ваш кода
-  // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i++) {
+    if (worker(arrOfArr[i]) > max) {
+      max = worker(arrOfArr[i]);
+    }
+  }
   return max;
 }
 
+const arrOfArr = [[1, 2, 3, 4], [10, 20, -10, -20], [100,23,44, -23], [-100,33]]
+console.log(makeWork(arrOfArr, worker))
+
+
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = Infinity;
+  let max = -Infinity;
+  let sum = 0;
+
+  for (let i=0; i<arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i]
+    }
+    if (arr[i] < min) {
+      min = arr[i]
+    }
+  }
+
+  return Math.abs(max - min);
 }
