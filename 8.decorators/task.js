3d8c616656b2f7f021840ab3e7b983518141f344
Наталья <lu_na87@mail.ru>
2022-10-06 22:37:24 +0300

    Домашнее задание 8

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,8 +1,52 @@
+// Задача 1
+
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = [];
+
+  function wrapper(...args) {
+    const hash = args.join(',');
+    let objectInCache = cache.find((item) => item.hash === hash);
+    if (objectInCache) {
+      console.log("Из кэша: " + objectInCache.value);
+      return "Из кэша: " + objectInCache.value;
+    }
+
+    let result = func(...args);
+    cache.push({ hash, value: result });
+    if (cache.length > 5) {
+      cache.shift();
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
+  }
+  return wrapper
 }
 
+function debounceDecoratorNew(func, ms) {
+  let timerId;
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+  return function wrapper(...args) {
+    if (timerId == undefined) {
+      func.apply(this, args);
+    }
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      func.apply(this, args)
+    }, ms);
+  }
+}
+
+function debounceDecorator2(func) {
+  let timerId;
+  wrapper.count = 0;
+  return function wrapper(...args) {
+    if (timerId == undefined) {
+      func.apply(this, args);
+      wrapper.count++;
+    }
+    clearTimeout(timerId);
+    timerId = setTimeout(() => {
+      func.apply(this, args)
+    }, ms);
+  }
 }
